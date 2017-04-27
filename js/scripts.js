998f471c1bb625b2f22ca3d65e984f2d3549110a
tomasz.kraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-04-27 22:38:11 +0200

    change else if into else

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function getTriangleArea(a, h) {
   if ((a > 0) && (h > 0)) {
     return a * h / 2;
-  } else if ((a < 0) || (h < 0)) {
+  } else ((a < 0) || (h < 0)) {
     return "Nieprawidłowe dane";
   }
 }
